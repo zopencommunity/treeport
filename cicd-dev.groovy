b2ea48b4f5102037c1ea39e7b7dfc305ae31c6db
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:37:06 -0400

    Change organization to zopencommunity

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/treeport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/treeport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/treeport.git'), string(name: 'PORT_DESCRIPTION', value: 'tree' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/treeport.git'), string(name: 'PORT_DESCRIPTION', value: 'tree' ), string(name: 'BUILD_LINE', value: 'DEV') ]
   }
 }
